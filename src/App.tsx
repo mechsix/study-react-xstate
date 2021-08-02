f1d4ea4ecd1f06453aadb5c1c12673cc5d8e2100
Add eslint into devDependencies <mech_tsai@trendmicro.com>
2021-08-03 00:25:08 +0800

    Add react-router; Move the first app to /#/app1

--- src/App.tsx
+++ src/App.tsx
@@ -1,116 +1,15 @@
 import React from 'react';
-import {useMachine} from '@xstate/react';
-import {
-  interpret,
-  createMachine,
-  assign,
-  actions,
-  send,
-  DoneInvokeEvent,
-} from 'xstate';
-import './App.css';
-
-
-const {cancel} = actions;
-
-type Context = {
-  phrase: string;
-  result: number | undefined;
-}
-
-type TypeEvent = {
-  type: 'TYPE',
-  data: string
-}
-
-type SearchEvent = {
-  type: 'SEARCH';
-  data: undefined;
-}
-
-type Events = TypeEvent | SearchEvent;
-
-const searchMachine = createMachine<Context, Events>(
-    {
-      id: 'main',
-      initial: 'idle',
-      context: {
-        phrase: '',
-        result: undefined,
-      },
-      on: {
-        TYPE: {
-          actions: ['setPhrase', 'cancelSearchEvent', 'sendSearchEvent'],
-        },
-        SEARCH: {
-          target: '.searching',
-        },
-      },
-      states: {
-        idle: {},
-        searching: {
-          invoke: {
-            src: 'search',
-            onDone: {
-              actions: 'setResult',
-              target: 'idle',
-            },
-            onError: {
-              target: 'idle',
-            },
-          },
-        },
-      },
-    },
-    {
-      actions: {
-        setPhrase: () => (
-          assign({
-            phrase: (_, event: TypeEvent) => event.data,
-          })),
-        sendSearchEvent: send(
-            {type: 'SEARCH'},
-            {id: 'searchEvent', delay: 10},
-        ),
-        setResult: assign({
-          result: (_, event: DoneInvokeEvent<number>) => event.data,
-          // https://github.com/statelyai/xstate/issues/1198
-          // eslint-disable-next-line @typescript-eslint/no-explicit-any
-        }) as any,
-        cancelSearchEvent: cancel('searchEvent'),
-      },
-      services: {
-        search: async () => {
-          return new Promise((resolve) => {
-            setTimeout(() => {
-              resolve(Math.random());
-            }, 1000);
-          });
-        },
-      },
-    },
-);
-
-
-const service = interpret(searchMachine, {devTools: true});
-service.start();
+import {HashRouter, Route, Switch} from "react-router-dom";
+import App1 from './apps/app1';
 
 const App: React.FC = () => {
-  const [current, send] = useMachine(searchMachine);
-  const {result, phrase} = current.context;
-  return (
-    <div>
-      Result: <strong>{result}</strong>
-      <br/>
-      State: <strong>{current.value}</strong>
-      <br/>
-      <input
-        value={phrase}
-        onChange={(event) => {
-          send({type: 'TYPE', data: event.target.value});
-        }}/>
-    </div>
-  );
-};
+    return (
+        <HashRouter>
+            <Switch>
+                <Route path={"/app1"} component={App1}/>
+            </Switch>
+        </HashRouter>
+    )
+}
 
 export default App;
